c2d104d757f2c9e73bd318eb620348415ee9a6be
Manuel Alejandro Moscoso Dominguez <manuel@mobiquos.cl>
2021-12-15 14:42:46 -0300

    Updated

--- contenido.php
+++ contenido.php
@@ -1,14 +1,14 @@
 <?php
 // Conectando, seleccionando la base de datos
-$link = mysqli_connect('localhost', 'id17218785_user_opcional', '7HGsw==]GW59WP{{
-    ','id17218785_opcional')
-    or die('No se pudo conectar: ' . mysql_error());
+$link = $link = mysqli_connect('localhost', 'user_opcional', 'pass_opcional','opcional') or die('No se pudo conectar: ' . mysqli_error());
+
 echo 'Connected successfully';
 
 //insertContenido
 if (isset($_POST['titulo']) && isset($_POST['autor']) && isset($_POST['descripcion']) && isset($_POST['url'])) {
     $query = "INSERT INTO contenido (titulo, autor,descripcion,url) values ('".$_POST['titulo']."','".$_POST['autor']."','".$_POST['descripcion']."','".$_POST['url']."')";
     $result = mysqli_query($link,$query) or die('Consulta fallida: ' . mysql_error());
-    header('Location: index.php');
+    //header('Location: index.php');
+    echo "<script type='text/javascript'>window.top.location='index.php';</script>"; exit;
 }
 ?>
